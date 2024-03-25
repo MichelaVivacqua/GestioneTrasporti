f33340c7a215acbb32469c71faa624a75bd290e6
MichelaVivacqua <vivacquamichela@gmail.com>
2024-03-25 20:26:07 +0100

    tessereDao

--- src/main/java/team4/Application.java
+++ src/main/java/team4/Application.java
@@ -3,11 +3,18 @@ package team4;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import team4.dao.TessereDAO;
+import team4.entities.Tessera;
+
+import java.time.LocalDate;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestionetrasporti");
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
+        TessereDAO tessereDAO = new TessereDAO(em);
 
+        Tessera tessera1 = new Tessera("Michela","Vivacqua", LocalDate.parse("25-03-2024"));
+        tessereDAO.save(tessera1);
     }
 }
--- src/main/java/team4/entities/Tessera.java
+++ src/main/java/team4/entities/Tessera.java
@@ -31,4 +31,44 @@ public class Tessera {
         this.dataEmissione = dataEmissione;
         this.dataScadenza = dataEmissione.plusYears(1);
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public LocalDate getDataEmissione() {
+        return dataEmissione;
+    }
+
+    public void setDataEmissione(LocalDate dataEmissione) {
+        this.dataEmissione = dataEmissione;
+    }
+
+    public LocalDate getDataScadenza() {
+        return dataScadenza;
+    }
+
+    public void setDataScadenza(LocalDate dataScadenza) {
+        this.dataScadenza = dataScadenza;
+    }
 }
