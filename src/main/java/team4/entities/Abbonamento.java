7b3408a09f947983f00ee4d297eebba8c035ea1a
GiuseppeAverna <151162626+GiuseppeAverna@users.noreply.github.com>
2024-03-26 16:40:51 +0100

    modifica
    modifica commit

--- src/main/java/team4/entities/Abbonamento.java
+++ src/main/java/team4/entities/Abbonamento.java
@@ -16,6 +16,22 @@ public class Abbonamento extends Biglietto implements Controllabile {
     @Enumerated(EnumType.STRING)
     @Column(name = "tipo_durata")
     private DURATA_TITOLO tipo;
+    private DURATA_TITOLO durata;
+
+    public Abbonamento() {}
+
+
+    public void setDurata(DURATA_TITOLO durata) {
+        this.durata = durata;
+    }
+
+    public Abbonamento(Mezzo mezzo, long emessoDa, LocalDate dataDiVidimazione, Tessera tessera, DURATA_TITOLO durata) {
+        super(mezzo, emessoDa, dataDiVidimazione, tessera);
+        this.durata = durata;
+    }
+    public DURATA_TITOLO getDurata() {
+        return durata;
+    }
 
     @Override
     public boolean isValido() {
